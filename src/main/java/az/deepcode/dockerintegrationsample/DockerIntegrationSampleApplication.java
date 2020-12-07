4fb749645871b3e2770d6641d1ec8247fbd84cfe
JEYHUN <jeyhunmustafazadeh@gmail.com>
2020-12-07 15:26:52 +0400

    First commit

---
+++ src/main/java/az/deepcode/dockerintegrationsample/DockerIntegrationSampleApplication.java
@@ -0,0 +1,13 @@
+package az.deepcode.dockerintegrationsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerIntegrationSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DockerIntegrationSampleApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/deepcode/dockerintegrationsample/DockerIntegrationSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package az.deepcode.dockerintegrationsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerIntegrationSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
